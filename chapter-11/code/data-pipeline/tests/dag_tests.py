34d9ebf979d87865e1c3468a9953fe341b3f93b6
adiwijaya <adiwijaya@google.com>
2021-11-06 12:00:30 +0800

    Update chapter 11

--- chapter-11/code/data-pipeline/tests/dag_tests.py
+++ chapter-11/code/data-pipeline/tests/dag_tests.py
@@ -1,28 +1,26 @@
 """
-DAG Integrity Tests
+DAG Unit Tests
 """
 
 import unittest
 from airflow.models import DagBag
 
-
 class TestDags(unittest.TestCase):
     """DAG Test Case"""
-
-    LOAD_THRESHOLD_SECONDS = 2
 
     def setUp(self):
         self.dagbag = DagBag()
 
-    def test_dags_syntax(self):
+    def test_dag_loaded(self):
         """Assert DAG bag load correctly"""
-        for key in self.dagbag.dags:
-            print(key)
-        self.assertFalse(
-            len(self.dagbag.import_errors),
-            f"DAG import errors. Errors: {self.dagbag.import_errors}")
-
-
+        print(len(self.dagbag.dags))
+        
+        for dag in self.dagbag.dags:
+            self.assertIsNotNone(dag, "DAG is empty: {}".format(dag))
+            print(dag)
+        
+        self.assertEqual(len(self.dagbag.import_errors), 0 , "DAG Errors: {}".format(self.dagbag.import_errors))
+            
 if __name__ == '__main__':
     SUITE = unittest.TestLoader().loadTestsFromTestCase(TestDags)
     unittest.TextTestRunner(verbosity=2).run(SUITE)
